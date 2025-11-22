e0e69b9d7522ca74eaa43fc99f8abb5bf76d70c0
Victor Eduardo <viceduartt@gmail.com>
2025-11-22 00:03:41 -0300

    v1

---
+++ main.js
@@ -0,0 +1,51 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+app.use(express.json());
+
+
+app.post("/sendEmail", async (req, res) => {
+  const { emailContact, bodyMsg } = req.body;
+
+  
+  try {
+      const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+              user: process.env.EMAIL,
+              pass: process.env.PASS
+            }
+        });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.SEND_FROM,
+      subject: "New client from portfolio: " + emailContact,
+      text: bodyMsg,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+
+    res.json({ status: "ok"});
+  } catch (err) {
+    res.json({ status: "error"});
+
+  }
+});
+
+
+app.listen(process.env.PORT, () => console.log("server run"));
